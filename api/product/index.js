99f18ec4c19c5fa6982e8e21f8e15677aba81247
Hoàng Nguyễn <76682879+HoangNguyen2907@users.noreply.github.com>
2022-01-16 19:33:37 +0700

    Delete api/product directory

--- api/product/index.js
+++
@@ -1,8 +0,0 @@
-const {Router} = require('express');
-
-const router = Router();
-const apiProductContronler = require("./apiProductContronler.js");
-
-router.post('/rate', apiProductContronler.rate);
-router.get('/:productid/rating', apiProductContronler.getRatings)
-module.exports = router;
